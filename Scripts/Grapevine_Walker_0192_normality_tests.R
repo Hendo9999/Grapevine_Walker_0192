beba633e2a15b24912cce7961294d29598b5fce1
Henderson <hen232@csiro.au>
2020-01-29 18:57:24 +1030

    added to normality analysis script

--- Scripts/Grapevine_Walker_0192_normality_tests.R
+++ Scripts/Grapevine_Walker_0192_normality_tests.R
@@ -5,6 +5,126 @@ source("Scripts/Grapevine_Walker_0192_load_tidy_data.R")
 #analyse normality in individual treatment groups
 
 
+#no heat & no salt
+nhns <- chloride_3_tidy %>% 
+  filter(treatment_heat == "no_heat", treatment_salt == "no_salt")
+
+nhns1 <- nhns$cl_dry_weight_calc
+
+#histogram
+hist(nhns1, main = " ", xlab = "Leaf chloride content", freq=TRUE)
+
+
+#density plot
+hist(nhns1, main = " ", xlab = "Leaf chloride content", freq=FALSE)
+curve(dnorm(x, mean=mean(nhns1), sd=sd(nhns1)), add=TRUE, col="red", lwd=2) 
+
+
+#Kernel Density plots (more effective way to  view distribution of variable then histogram?)
+plot(density(nhns1))
+curve(dnorm(x, mean=mean(nhns1), sd=sd(nhns1)), add=TRUE, col="red", lwd=2) 
+
+
+#qq plots
+
+qqnorm(nhns1)
+qqline(nhns1, col="red")
+
+
+
+
+#heat & salt
+
+hs <- chloride_3_tidy %>% 
+  filter(treatment_heat == "heat", treatment_salt == "salt")
+
+hs1 <- hs$cl_dry_weight_calc
+
+
+#histogram
+hist(hs1, main = " ", xlab = "Leaf chloride content", freq=TRUE)
+
+#density plot
+hist(hs1, main = " ", xlab = "Leaf chloride content", freq=FALSE)
+
+curve(dnorm(x, mean=mean(hs1), sd=sd(hs1)), add=TRUE, col="red", lwd=2) 
+
+
+#Kernel Density plots (more effective way to  view distribution of variable then histogram?)
+plot(density(hs1))
+curve(dnorm(x, mean=mean(hs1), sd=sd(hs1)), add=TRUE, col="red", lwd=2) 
+
+
+#qq plots
+
+qqnorm(hs1)
+qqline(hs1, col="red")
+
+
+
+
+
+#noheat & salt
+
+nhs <- chloride_3_tidy %>% 
+  filter(treatment_heat == "no_heat", treatment_salt == "salt")
+
+nhs1 <- nhs$cl_dry_weight_calc
+
+
+#histogram
+hist(nhs1, main = " ", xlab = "Leaf chloride content", freq=TRUE)
+
+#density plot
+hist(nhs1, main = " ", xlab = "Leaf chloride content", freq=FALSE)
+
+curve(dnorm(x, mean=mean(nhs1), sd=sd(nhs1)), add=TRUE, col="red", lwd=2) 
+
+
+#Kernel Density plots (more effective way to  view distribution of variable then histogram?)
+plot(density(nhs1))
+curve(dnorm(x, mean=mean(nhs1), sd=sd(nhs1)), add=TRUE, col="red", lwd=2) 
+
+
+#qq plots
+
+qqnorm(nhs1)
+qqline(nhs1, col="red")
+
+
+
+#heat & nosalt
+
+hns <- chloride_3_tidy %>% 
+  filter(treatment_heat == "heat", treatment_salt == "no_salt")
+
+hns1 <- hns$cl_dry_weight_calc
+
+
+#histogram
+hist(hns1, main = " ", xlab = "Leaf chloride content", freq=TRUE)
+
+#density plot
+hist(hns1, main = " ", xlab = "Leaf chloride content", freq=FALSE)
+
+curve(dnorm(x, mean=mean(hns1), sd=sd(hns1)), add=TRUE, col="red", lwd=2) 
+
+
+#Kernel Density plots (more effective way to  view distribution of variable then histogram?)
+plot(density(hns1))
+curve(dnorm(x, mean=mean(hns1), sd=sd(hns1)), add=TRUE, col="red", lwd=2) 
+
+
+#qq plots
+
+qqnorm(hns1)
+qqline(hns1, col="red")
+
+
+
+
+
+
 
 
 #Normality tests
